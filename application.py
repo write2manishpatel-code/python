a8987071ce54af452b10b4af283fbc3e10b7856d
write2manishpatel-code <write2manishpatel@gmail.com>
2026-05-06 16:41:09 +1000

    Add files via upload

---
+++ application.py
@@ -0,0 +1,25 @@
+a=10
+b=12
+c=0
+
+
+if 12 < 0 or 12 > 4:
+    print("one number is greater than 0")
+else:
+    print("no number is greater than 0")
+a=10
+b=12
+
+print(not a ==b)
+
+b = 12
+c = 12
+
+print(not b == c)
+
+
+a = "python"
+b = "coding"
+
+if not a ==b:
+    print(a, 'and', b, 'are different.')
